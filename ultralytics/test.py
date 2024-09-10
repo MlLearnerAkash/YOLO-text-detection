dcc8454be26244e34ad2adc378d52736cf1bdba1
MlLearnerAkash <manna.phys@gmail.com>
2024-09-10 07:32:22 +0000

    modifies test

--- ultralytics/test.py
+++ ultralytics/test.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 
 # # Load a model
 model = YOLO("/home/akash/ws/YOLO-text-detection/ultralytics/runs/detect/hindi_finetune_base_model_090924/weights/best.pt")  # pretrained YOLOv8n model
-source = "/home/akash/ws/YOLO-text-detection/ultralytics/Hindi-HW_1.png"
+source = "/home/akash/ws/dataset/hand_written/test_data/book_851_ravi_style_gt/ravi_style_gt/images"
 # Run batched inference on a list of images
 results = model(source= source,
                 conf=0.15, iou= 0.15, save_txt= True, save = True)  # return a list of Results objects
